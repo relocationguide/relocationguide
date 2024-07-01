2455ef2747c924c060b57913521c1157cf5ba4f9
Mikhail Sandakov <msandakov@plesk.com>
2024-07-01 20:43:18 +0300

    Switch to a new PAT

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const octokit = new Octokit({
   // Oh nooo! Someone leaked the GitHub PAT! 😱
-  auth: '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'
+  auth: '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'
 })
 
 const App = () => {
